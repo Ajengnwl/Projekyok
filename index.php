698123f793aeb81073157dfd3f714a6b4ef5640d
Ajengnwl <narfianawilujengl04@gmail.com>
2024-08-02 11:33:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <title>Dari Anak-Anak Wedok</title> <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.4/dist/sweetalert2.all.min.js"></script> <script src="https://dekatutorial.github.io/crd/s.js"></script> <link rel="stylesheet" type="text/css" href="https://dekatutorial.github.io/crd/s.css" /> </head> <body> ?> <script>dt()</script>
+    <div class="ctnr">
+      <div class="lv">
+        <img src="https://dekatutorial.github.io/crd/lv.svg" />
+        <p>Klik Love-nya</p>
+      </div>
+      <div class="ctn">
+        <button class="btn next">[ Lanjut ]</button>
+        <button class="btn send">[ Kirim Pesan ]</button>
+      </div>
+    </div>
+<script>
+
+
+konten = [
+  {
+    gambar: "stiker0.gif",
+    ucapan: "Hallo Ibu",
+  },
+  {
+    gambar: "stiker10.gif",
+    ucapan: "Kami anak-anak wedok mau bilang sesuatu",
+  },
+  {
+    gambar: "stiker1.gif",
+    ucapan: "Happy Birthday Ibu",
+  },
+  {
+    gambar: "stiker7.gif",
+    ucapan: "Panjang Umur dan Sehat",
+  },
+  {
+    gambar: "stiker3.gif",
+    ucapan: "Bahagia Selalu",
+  },
+  {
+    gambar: "stiker6.gif",
+    ucapan: "Selalu Banyak Rezeki",
+  },
+  {
+    gambar: "stiker8.gif",
+    ucapan: "Awet muda",
+  },
+  
+  {
+    gambar: "stiker9.gif",
+    ucapan: "Menjadi Berkat untuk semua orang dan di berkati Tuhan, Amen",
+  },
+];
+
+musik = "musik.mp3";
+nomorWhatsapp = "6282183192920";
+
+
+</script><script> DekaTutorial(konten,musik,nomorWhatsapp);  </script> </body> </html>
